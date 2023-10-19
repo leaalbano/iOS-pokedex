3f2eceb99c49636e6f3a324298b4df27d296197b
Saul Andrade <saulandrade124@csu.fullerton.edu>
2023-10-18 20:40:33 -0700

    made background and added text

--- pokedex/pokedex/CharmanderViewController.swift
+++ pokedex/pokedex/CharmanderViewController.swift
@@ -27,9 +27,27 @@ class CharmanderViewController: UIViewController{
             for: UIControl.State.normal
         )
         
+        //Background View
+        self.view.backgroundColor = UIColor(red: 43/255.0, green: 41/255.0, blue: 44/255.0, alpha: 1)
         
+        //HEADER FRAME
+        let headerFrame = CGRect(x: 0 , y: 0 , width: 393 , height: 203)
+        let HeaderView = UIView(frame: headerFrame)
+        HeaderView.backgroundColor = UIColor(red: 212/255.0, green: 59/255.0, blue: 71/255.0, alpha: 1)
+        view.addSubview(HeaderView)
+        
+        //TEXT IN HEADER (CHARMANDER LABEL 
+        let pokemonCharater = UILabel(frame: CGRectMake(20, 80, 353, 39.33))
+        pokemonCharater.font = UIFont.systemFont(ofSize:31 , weight: .bold)
+        pokemonCharater.textColor = UIColor.white
+        pokemonCharater.text = "Charmander"
+        
+        view.addSubview(pokemonCharater)
+        
+        
+        //Button Config
         self.button.backgroundColor = UIColor.systemRed
-        
+        //Button location
         self.button.frame = CGRect(
             x: 20, y: 20,
             width: 100, height: 100
@@ -45,7 +63,7 @@ class CharmanderViewController: UIViewController{
         self.view.addSubview(self.button)
         
         
-        
+        //button constraints
         let bCenterX = self.button.centerXAnchor.constraint(
             equalTo: self.view.safeAreaLayoutGuide.centerXAnchor
         )
